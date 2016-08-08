46bc5249e14d32157e3fef67d42d5df1d2d20871
JhonyPalencia <JhonyPalencia@users.noreply.github.com>
2016-08-07 23:02:27 -0500

    Actualizacion de proyecto - Jose

--- app/src/main/java/com/example/hp_user/ubizone/customSwip.java
+++ app/src/main/java/com/example/hp_user/ubizone/customSwip.java
@@ -6,39 +6,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
-/**
- * Created by HP-USER on 26/07/2016.
- */
-public class customSwip extends PagerAdapter {
-    private int [] imageResources ={R.drawable.imagen1,R.drawable.imagen2,R.drawable.imagen3};
+public class CustomSwip extends PagerAdapter {
+    private int[] imageResources = {R.drawable.imagen1, R.drawable.imagen2, R.drawable.imagen3};
     private Context ctx;
     private LayoutInflater layoutInflater;
 
-    public customSwip(Context c) {
+    public CustomSwip(Context c) {
         ctx=c;
     }
 
-
     @Override
     public int getCount() {
+
         return imageResources.length;
     }
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
-        layoutInflater= (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View itemView=layoutInflater.inflate(R.layout.activity_custom_swip,container,false);
-        ImageView imageView=(ImageView) itemView.findViewById(R.id.swip_image_view);
-        TextView textView=(TextView) itemView.findViewById(R.id.imageCount);
+        layoutInflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemView = layoutInflater.inflate(R.layout.activity_custom_swip, container, false);
+        ImageView imageView = (ImageView) itemView.findViewById(R.id.swip_image_view);
+
         imageView.setImageResource(imageResources[position]);
-        textView.setText("Barranquilla" );
+
         container.addView(itemView);
         return itemView;
     }
-
-
 
 
     @Override
@@ -49,6 +43,6 @@ public class customSwip extends PagerAdapter {
     @Override
     public boolean isViewFromObject(View view, Object object) {
 
-        return  (view==object);
+        return (view == object);
     }
 }
